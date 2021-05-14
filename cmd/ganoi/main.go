c3d8968c0061c621ca6facb6020a4248d80342f9
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-05-14 10:19:22 +0200

    refactp

--- cmd/ganoi/main.go
+++ cmd/ganoi/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"github.com/jtbonhomme/ganoi/internal/game"
+	"github.com/jtbonhomme/ganoi"
 )
 
 func main() {
-	g := game.New(4)
-
-	/*    go func() {
-	      for i := 0 ; i < 10; i++ {
-	          base4.SetPosition(10, 10+i)
-	      }
-	  }()*/
-	g.Start()
+	ganoi.Run()
 }
