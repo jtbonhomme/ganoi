d90d3b4119ea20bfdbe5fd055c9d2553ff3b09c6
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-05-14 08:42:54 +0200

    add game struct

--- main.go
+++ main.go
@@ -4,37 +4,65 @@ import (
     tl "github.com/JoelOtter/termloop"
 )
 
-func main() {
-    game := tl.NewGame()
-    game.Screen().SetFps(60)
+type Tower struct {
+    Name string
+    Pos int
+    Height int
+}
 
+type Game struct {
+    Game *tl.Game
+    Towers []Tower
+    Level  *tl.BaseLevel
+}
+
+func New(n int) *Game {
+    _game := tl.NewGame()
     level := tl.NewBaseLevel(tl.Cell{
         Bg: tl.ColorBlack,
         Fg: tl.ColorWhite,
         Ch: ' ',
     })
+    towers := make([]Tower, n)
+
+    game := Game{
+        Game: _game,
+        Level: level,
+        Towers: towers,
+    }
 
     tower1 := tl.NewRectangle(27, 5, 1, 6, tl.ColorWhite)
     tower2 := tl.NewRectangle(52, 5, 1, 6, tl.ColorWhite)
     tower3 := tl.NewRectangle(77, 5, 1, 6, tl.ColorWhite)
+
+    game.Level.AddEntity(tl.NewText(2, 2, "HANOI TOWERS", tl.ColorWhite, tl.ColorBlack))
+
+    game.Level.AddEntity(tower1)
+    game.Level.AddEntity(tower2)
+    game.Level.AddEntity(tower3)
 
     base4 := tl.NewRectangle(17, 10, 21, 1, tl.ColorBlue)
     base3 := tl.NewRectangle(20, 9, 15, 1, tl.ColorGreen)
     base2 := tl.NewRectangle(23, 8, 9, 1, tl.ColorRed)
     base1 := tl.NewRectangle(26, 7, 3, 1, tl.ColorCyan)
 
-    level.AddEntity(tl.NewText(2, 2, "HANOI TOWERS", tl.ColorWhite, tl.ColorBlack))
+    game.Level.AddEntity(base4)
+    game.Level.AddEntity(base3)
+    game.Level.AddEntity(base2)
+    game.Level.AddEntity(base1)
 
-    level.AddEntity(tower1)
-    level.AddEntity(tower2)
-    level.AddEntity(tower3)
+    game.Game.Screen().SetLevel(game.Level)
 
-    level.AddEntity(base4)
-    level.AddEntity(base3)
-    level.AddEntity(base2)
-    level.AddEntity(base1)
+    return &game
+}
 
-    game.Screen().SetLevel(level)
+func (g *Game) Start() {
+    g.Game.Start()
+}
+
+func main() {
+    game := New(4)
+
 /*    go func() {
         for i := 0 ; i < 10; i++ {
             base4.SetPosition(10, 10+i)
